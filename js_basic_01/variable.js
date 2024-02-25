10f88f00d670e9b7176fde5e6fbeb35ac3896146
anshraval21 <136162100+anshraval21@users.noreply.github.com>
2024-02-25 12:44:35 +0000

    final commit

---
+++ js_basic_01/variable.js
@@ -0,0 +1,14 @@
+const accId = 1234
+let accEmail = "ansh123@gmail.com"
+var accPassword = "93456"
+accCity = "Jaipur"
+let accState
+
+//accId = 4566
+accEmail = "dhqpl34@yahoo.com"
+accPassword = "212121"
+accCity = "Bhavnagar"
+
+console.log(accId);
+
+console.table([accId, accEmail, accPassword, accCity, accState])
