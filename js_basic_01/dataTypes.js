0f6079f57644bd08185f8fbfd579a3edbc2cbc97
anshraval21 <136162100+anshraval21@users.noreply.github.com>
2024-02-25 13:07:53 +0000

    datatypes

---
+++ js_basic_01/dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+// alert( 3 + 3) used in browser// we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high //should not write like this
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => for large no.
+// string => ""
+// boolean => true/false
+// null => standalone value // it is object
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
